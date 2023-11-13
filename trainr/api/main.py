e7ad7da440530bb2e025fe9c7b42d9b0e106e09e
mgorsk1 <gorskimariusz13@gmail.com>
2023-11-13 17:54:03 +0100

    major refactor overhaul

--- trainr/api/main.py
+++ trainr/api/main.py
@@ -1,13 +1,41 @@
+import threading
+import time
+
+import uvicorn
 from fastapi import FastAPI
 
 from trainr.api.v1 import v1
-from trainr.api.v1.models.health import Health
+from trainr.api.v1.model.health import HealthApiModel
+
+
+# class BackgroundTasks(threading.Thread):
+#     def run(self, *args, **kwargs):
+#         val = 5
+#         while True:
+#             val = val + 5
+#             requests.post('localhost:1337/api/v1/hr/', params=dict(value=val + 5))
+#
+#             # Set fan
+#
+#             requests.post('localhost:1337/api/v1/light')
+#
+#             # Set light
+#
+#
+#             time.sleep(5)
+#
 
 app = FastAPI()
 
 app.include_router(v1, prefix='/api')
 
 
-@app.get("/", response_model=Health)
+@app.get("/", response_model=HealthApiModel)
 async def root():
-    return Health(healthy=True)
+    return HealthApiModel(healthy=True)
+
+
+# if __name__ == '__main__':
+#     t = BackgroundTasks()
+#     t.start()
+#     uvicorn.run(app)
